c4efc32f1316bf96ec0535d9598b59f8c9fe73c6
Daniil Dybka <creagoooff@gmail.com>
2023-08-27 13:54:36 +0300

    Work at share post

---
+++ src/app/js/share-post.js
@@ -0,0 +1,15 @@
+function ShareTG() {
+	console.log("share tg");
+}
+
+function ShareVK() {
+	console.log("share tg");
+}
+
+function ShareOK() {
+	console.log("share tg");
+}
+
+function ShareLink() {
+	console.log("share tg");
+}
