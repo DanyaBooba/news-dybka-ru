0e510f11886e938d2d7bbd5544bc44b1b812db98
Daniil Dybka <creagoooff@gmail.com>
2023-08-28 16:17:28 +0300

    Work at share post

--- src/app/js/share-post.js
+++ src/app/js/share-post.js
@@ -1,15 +1,30 @@
 function ShareTG() {
-	console.log("share tg");
+	window.open(
+		"https://telegram.me/share/url?url=" +
+			SpecialURL() +
+			"&text=" +
+			document.title,
+		"_blank"
+	);
 }
 
 function ShareVK() {
-	console.log("share tg");
+	window.open("https://vk.com/share.php?url=" + SpecialURL(), "_blank");
 }
 
 function ShareOK() {
-	console.log("share tg");
+	window.open(
+		"https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=" +
+			SpecialURL(),
+		"_blank"
+	);
 }
 
 function ShareLink() {
-	console.log("share tg");
+	let text = SpecialURL();
+	navigator.clipboard.writeText(text);
+}
+
+function SpecialURL() {
+	return "https://news.dybka.ru" + window.location.pathname;
 }
