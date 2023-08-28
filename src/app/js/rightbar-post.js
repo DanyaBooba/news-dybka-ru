c60a69f4a284f9d6416da04dff2cbcc4c268fef9
Daniil Dybka <creagoooff@gmail.com>
2023-08-29 00:05:14 +0300

    Add posts to js

--- src/app/js/rightbar-post.js
+++ src/app/js/rightbar-post.js
@@ -1,69 +1,141 @@
 var pages = [
 	{
-		name: "Название поста",
+		name: "Русские Карты 2",
+		class: "Игры",
+		url: "russian-cards-2",
+		alt: "Шапка игры Русские Карты 2",
+	},
+	{
+		name: "Babka On The Hunt",
+		class: "Игры",
+		url: "babka-on-the-hunt",
+		alt: "Шапка игры Бабушка На Охоте",
+	},
+	{
+		name: "Sweetness",
 		class: "Игры",
 		url: "sweetness",
 		alt: "Шапка игры Sweetness",
 	},
 	{
-		name: "Название поста",
+		name: "Запомни Эти Карты",
 		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		url: "remember-these-cards",
+		alt: "Шапка игры Запомни Эти Карты",
 	},
 	{
-		name: "Название поста",
+		name: "Камень Ножницы Бумага",
 		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		url: "rock-paper-scissors",
+		alt: "Шапка игры Камень Ножницы Бумага",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "Макбук 2017 — очень доволен!",
+		class: "Техника",
+		url: "macbook-2017-review",
+		alt: "Макбук 2017 года на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "AirPods Pro — стоит купить?",
+		class: "Техника",
+		url: "airpods-pro-is-it-worth-the-money",
+		alt: "AirPods Pro на черном фоне",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "iPhone Xr",
+		class: "Техника",
+		url: "the-new-generation-of-the-iphone-xr-device",
+		alt: "iPhone Xr на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "iPad mini 4",
+		class: "Техника",
+		url: "ipad-mini-4-is-an-ambiguous-device",
+		alt: "iPad mini 4 на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "MacBook Pro 2014",
+		class: "Техника",
+		url: "macbook-pro-2014-13-review",
+		alt: "Макбук 2014 года на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "Какие макбуки действительно стоит брать?",
+		class: "Техника",
+		url: "which-macbooks-are-really-worth-getting",
+		alt: "Два макбука на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "Опыт использования iPhone 6S",
+		class: "Техника",
+		url: "experience-using-iphone-6s-for-six-months",
+		alt: "iPhone 6S на столе",
 	},
 	{
-		name: "Название поста",
-		class: "Игры",
-		url: "sweetness",
-		alt: "Шапка игры Sweetness",
+		name: "Разница в макбуках за 3 года",
+		class: "Техника",
+		url: "difference-in-macbooks-for-3-years",
+		alt: "Два макбука на столе",
+	},
+	{
+		name: "PHP ЖИВ?",
+		class: "Разное",
+		url: "php-is-alive",
+		alt: "Логотип PHP",
+	},
+	{
+		name: "Как успешно продавать на Б/У рынках",
+		class: "Разное",
+		url: "how-to-sell-on-buy-markets",
+		alt: "Портмоне на фоне ноутбука",
+	},
+	{
+		name: "Общение с клиентами в интернете",
+		class: "Разное",
+		url: "communication-with-clients-on-the-internet",
+		alt: "Открытая почта",
+	},
+	{
+		name: "Б/У рынок: что это, с чем его едят?",
+		class: "Разное",
+		url: "used-market-what-is-it-with-what-it-is-eaten",
+		alt: "Купюры в руке",
+	},
+	{
+		name: "Путь программиста — это трудный путь?",
+		class: "Разное",
+		url: "the-path-of-a-programmer-is-a-difficult-path",
+		alt: "Ноутбук на столе",
+	},
+	{
+		name: "История live.creagoo.ru",
+		class: "Разное",
+		url: "project-development-history-live-creagoo-ru-its-forced-closure",
+		alt: "Фотография логотипа Creagoo",
+	},
+	{
+		name: "С чего я начал изучать программирование",
+		class: "Разное",
+		url: "how-did-i-start-learning-programming",
+		alt: "Открытый исходный код",
+	},
+	{
+		name: "Как я борюсь против выгорания",
+		class: "Разное",
+		url: "how-i-fight-against-burnout",
+		alt: "Девушка выгорела на фото",
+	},
+	{
+		name: "Как я разрабатываю сайты на PHP",
+		class: "Разное",
+		url: "how-i-develop-websites-in-php",
+		alt: "Открытый исходный код",
+	},
+	{
+		name: "Разработка pet-проектов — что это и для чего?",
+		class: "Разное",
+		url: "pet-project-development-what-is-it-and-for-what",
+		alt: "Открытый исходный код",
 	},
 ];
 
@@ -75,7 +147,7 @@ function AddPost() {
 	var count_items = CountItems();
 
 	for (var i = 0; i < count_items; i++) {
-		var index = 0;
+		var index = GetIndex();
 
 		var block = pages[index];
 
@@ -126,6 +198,14 @@ function CountItems() {
 	if (count > 3) count = 3;
 
 	return count;
+}
+
+function GetIndex() {
+	return randomInteger(pages.length);
+}
+
+function randomInteger(max) {
+	return Math.floor(Math.random() * (max - 1));
 }
 
 AddPost();
