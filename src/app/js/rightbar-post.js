3339e3d744f15c65bd1cdb0de17110fc2d0d8a22
Daniil Dybka <creagoooff@gmail.com>
2023-08-28 16:38:18 +0300

    Work at right bar

--- src/app/js/rightbar-post.js
+++ src/app/js/rightbar-post.js
@@ -1,34 +1,69 @@
 var pages = [
-	"1232131232132131233213213",
-	"1232131232132131233213213",
-	"123",
-	"123",
-	"123",
-	"123",
+	{
+		name: "Название поста",
+		class: "Игры",
+		url: "sweetness",
+		alt: "Шапка игры Sweetness",
+	},
+	{
+		name: "Название поста",
+		class: "Игры",
+		url: "sweetness",
+		alt: "Шапка игры Sweetness",
+	},
+	{
+		name: "Название поста",
+		class: "Игры",
+		url: "sweetness",
+		alt: "Шапка игры Sweetness",
+	},
 ];
 
 function AddPost() {
-	// <div class="col pb-3 mb-sm-0">
-	//     <a href="/sweetness/" class="card card--link card--rounded border-0">
-	//         <img src="/sweetness/cap.jpg" class="card-img" alt="Логотип PHP">
-	//         <div class="card-img-overlay card--bg-gradient d-flex">
-	//             <div class="d-flex mt-auto flex-column">
-	//                 <p class="card-subtitle">
-	//                     Игры
-	//                 </p>
-	//                 <p class="card-title text-white card--title card--title-mini">
-	//                     Sweetness
-	//                 </p>
-	//             </div>
-	//         </div>
-	//     </a>
-	// </div>
-
 	var content = document.getElementById("post--right");
 
 	if (content == null) return;
 
-	console.log("add");
+	var count_items = 3;
+
+	if (count_items > pages.length) count_items = pages.length;
+
+	for (var i = 0; i < count_items; i++) {
+		var index = 0;
+
+		var block = pages[index];
+
+		pages.splice(index, 1);
+
+		var htmlblock =
+			`<div class="col pb-3 mb-sm-0">
+            <a href="/` +
+			block["url"] +
+			`/" class="card card--link card--rounded border-0">
+                <img src="/` +
+			block["url"] +
+			`/cap.jpg" class="card-img" alt="` +
+			block["alt"] +
+			`">
+                <div class="card-img-overlay card--bg-gradient d-flex">
+                    <div class="d-flex mt-auto flex-column">
+                        <p class="card-subtitle">
+                            ` +
+			block["class"] +
+			`
+                        </p>
+                        <p class="card-title text-white card--title card--title-mini">
+                            ` +
+			block["name"] +
+			`
+                        </p>
+                    </div>
+                </div>
+            </a>
+            </div>`;
+
+		content.insertAdjacentHTML("beforeend", htmlblock);
+	}
 }
 
 AddPost();
