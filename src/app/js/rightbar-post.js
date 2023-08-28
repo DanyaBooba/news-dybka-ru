44a6b77b03b930c7edfd4c98d63f431f7e7b88ac
Daniil Dybka <creagoooff@gmail.com>
2023-08-28 16:22:33 +0300

    Add js for right block at post

---
+++ src/app/js/rightbar-post.js
@@ -0,0 +1,34 @@
+var pages = [
+	"1232131232132131233213213",
+	"1232131232132131233213213",
+	"123",
+	"123",
+	"123",
+	"123",
+];
+
+function AddPost() {
+	// <div class="col pb-3 mb-sm-0">
+	//     <a href="/sweetness/" class="card card--link card--rounded border-0">
+	//         <img src="/sweetness/cap.jpg" class="card-img" alt="Логотип PHP">
+	//         <div class="card-img-overlay card--bg-gradient d-flex">
+	//             <div class="d-flex mt-auto flex-column">
+	//                 <p class="card-subtitle">
+	//                     Игры
+	//                 </p>
+	//                 <p class="card-title text-white card--title card--title-mini">
+	//                     Sweetness
+	//                 </p>
+	//             </div>
+	//         </div>
+	//     </a>
+	// </div>
+
+	var content = document.getElementById("post--right");
+
+	if (content == null) return;
+
+	console.log("add");
+}
+
+AddPost();
