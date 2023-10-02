0b7eecb3ede62d93769eb2e0b46e222a75c629fd
Daniil Dybka <creagoooff@gmail.com>
2023-10-02 18:14:32 +0300

    Work at change size text

--- src/app/js/changesize-post.js
+++ src/app/js/changesize-post.js
@@ -1,15 +1,74 @@
 function ChangeSize(value) {
+	let content = document.getElementById("post--main");
+	let btndefault = document.getElementById("textsizetodefault");
+
+	if (btndefault !== null) btndefault.classList.remove("d-none");
+
+	if (content === null) return;
+	ClearChangeSize(content);
+
 	if (value == 0) {
-		console.log("Default");
+		if (btndefault !== null) btndefault.classList.add("d-none");
 	} else if (value == -1) {
-		console.log("-1");
+		content.classList.add("downtext-1");
 	} else if (value == -2) {
-		console.log("-2");
+		content.classList.add("downtext-2");
 	} else if (value == 1) {
-		console.log("+1");
+		content.classList.add("uptext-1");
 	} else if (value == 2) {
-		console.log("+2");
+		content.classList.add("uptext-2");
 	}
 }
 
-ChangeSize(localStorage.getItem("textsize"));
+function TextDown() {
+	var value = localStorage.getItem("textsize");
+
+	if (value === null) value = -1;
+
+	if (value < -2 || value > 2) value = 0;
+
+	if (value > -2) value -= 1;
+
+	localStorage.setItem("textsize", value);
+
+	console.log(value);
+
+	ChangeSize(value);
+}
+
+function TextUp() {
+	var value = localStorage.getItem("textsize");
+
+	if (value === null) value = 1;
+
+	if (value < -2 || value > 2) value = 0;
+
+	if (value < 2) value++;
+
+	localStorage.setItem("textsize", value);
+
+	console.log(value);
+
+	ChangeSize(value);
+}
+
+function TextDefault() {
+	localStorage.setItem("textsize", 0);
+
+	console.log(0);
+
+	ChangeSize(0);
+}
+
+function ClearChangeSize(content) {
+	content.classList.remove("uptext-1");
+	content.classList.remove("uptext-2");
+	content.classList.remove("downtext-1");
+	content.classList.remove("downtext-2");
+}
+
+ChangeSize(
+	localStorage.getItem("textsize") !== null
+		? localStorage.getItem("textsize")
+		: 0
+);
