f7673981cb6d66ccab13cd16a5958e050ac6ad99
Daniil Dybka <creagoooff@gmail.com>
2023-10-02 17:12:13 +0300

    Work at size post

--- src/app/js/changesize-post.js
+++ src/app/js/changesize-post.js
@@ -1,5 +1,15 @@
-function ChangeSize() {
-	console.log("Change size");
+function ChangeSize(value) {
+	if (value == 0) {
+		console.log("Default");
+	} else if (value == -1) {
+		console.log("-1");
+	} else if (value == -2) {
+		console.log("-2");
+	} else if (value == 1) {
+		console.log("+1");
+	} else if (value == 2) {
+		console.log("+2");
+	}
 }
 
-ChangeSize();
+ChangeSize(localStorage.getItem("textsize"));
