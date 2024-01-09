27a83d727f9b3db383659f8ce86cac5fe04b34bd
Daniil Dybka <danil.dybko@gmail.com>
2024-01-09 03:31:25 +0300

    Add work tags

--- src/app/js/post-tags.js
+++ src/app/js/post-tags.js
@@ -1,1 +1,24 @@
-function Tag() {}
+let techmacbook = false;
+
+function TagTechMacbook() {
+	Tag(techmacbook, "have-macbook", "tag_techmacbook");
+
+	techmacbook = !techmacbook;
+}
+
+function Tag(active, itemid, buttonid) {
+	document.querySelectorAll(".col.post").forEach((item) => {
+		if (active) {
+			item.classList.remove("d-none");
+		} else {
+			if (item.id === itemid) {
+				item.classList.remove("d-none");
+			} else {
+				item.classList.add("d-none");
+			}
+		}
+	});
+
+	let button = document.getElementById(buttonid);
+	!active ? button.classList.add("active") : button.classList.remove("active");
+}
