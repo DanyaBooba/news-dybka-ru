e03892c64560b51774adacd65122f76fb3a9d1aa
Daniil Dybka <danil.dybko@gmail.com>
2024-06-29 02:37:09 +0300

    Работает над стилями

--- src/app/js/post/right-bar/margin.js
+++ src/app/js/post/right-bar/margin.js
@@ -1,15 +1,35 @@
-function addMarginToRightBar() {
-    const lineHeight = 48
-
+const marginStyle = () => {
+    const lineHeight = 38 * 0.95
+    const startMargin = lineHeight + 22
     const title = document.querySelector("h1")
-	if (!title) return
 
-	const countLines = Math.round(Number(title.offsetHeight) / lineHeight)
+    const countLines = Math.round(Number(title.offsetHeight) / lineHeight) - 1
+    return parseInt(countLines > 0 ? startMargin + lineHeight * countLines : 0)
+}
 
+const marginClass = () => {
+    const lineHeight = 48
+    const title = document.querySelector("h1")
+
+    const countLines = Math.round(Number(title.offsetHeight) / lineHeight) - 1
+	return `post--right-margin-${countLines}`
+}
+
+function addMarginToRightBar(styleValue) {
     const rightbar = document.getElementById("post--right")
     if (!rightbar) return
 
-	countLines > 1 ? rightbar.classList.add(`post--right-margin-${countLines}`) : null
+    rightbar.style.marginTop = `${styleValue}px`
 }
 
-addMarginToRightBar()
+function addMarginToShare(styleValue) {
+    const sharebar = document.querySelector('.post--share')
+    if (!sharebar) return
+
+    sharebar.style.marginTop = `${styleValue}px`
+}
+
+const styleValue = marginStyle()
+
+addMarginToRightBar(styleValue)
+addMarginToShare(styleValue)
