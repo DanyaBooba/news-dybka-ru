3a29b84029495e848423a4c8b9b957aa68e923a9
Daniil Dybka <danil.dybko@gmail.com>
2025-04-01 22:50:55 +0300

    Работает над дизайном

--- src/app/js/nav/active.js
+++ src/app/js/nav/active.js
@@ -1,10 +1,20 @@
+function setActiveElement(idName) {
+    const urlSplit = String((new URL(window.location)).pathname).split('/')
+    const url = urlSplit[urlSplit.length - 2]
+    const elementActive = document.getElementById(`${idName}-${url}`)
+
+    if (!elementActive) return
+
+    elementActive.classList.add('active')
+}
+
 function navActiveLinks() {
-    const url = String((new URL(window.location)).pathname).split('/')[1]
-    const elementActive = document.getElementById(`nav-${url}`)
+    setActiveElement('nav')
+}
 
-	if (!elementActive) return
-
-	elementActive.classList.add('active')
+function offcanvasActiveLinks() {
+    setActiveElement('offcanvas')
 }
 
 navActiveLinks()
+offcanvasActiveLinks()
